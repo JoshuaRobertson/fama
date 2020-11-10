ac1c49c919a6a931c48c3ebcb051612ea3dc84a6
JoshuaRobertson <josh.robertson@coinclear.uk>
2020-11-10 09:14:23 +0000

    Added functions & project folder, with base JS

---
+++ functions/fama.js
@@ -0,0 +1,43 @@
+// Logic testing
+exports.handler = function(event, context, callback) {
+	return {
+		statusCode: 200,
+		body: 'It is alive!'
+	}
+}
+
+// Set the status
+function setStatus() {
+  let emoji,
+      text;
+
+  switch () {
+    case 1:
+      emoji = ':pizza:';
+      text = 'Having lunch';
+      break;
+    case 2:
+      emoji = ':call_me_hand:';
+      text = 'On a call';
+      break;
+    case 3:
+      emoji = ':timer_clock:';
+      text = 'Be right back';
+      break;
+    case 4:
+      emoji = ':poop:';
+      text = 'Occupied';
+      break;
+    default:
+      emoji = '';
+      text = '';
+      break;
+  }
+
+  return {
+    profile: {
+      status_emoji: emoji,
+      status_text: text
+    }
+  }
+}
