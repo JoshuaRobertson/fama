2fb1052bd266723a3861f68637205ee38cd246ff
JoshuaRobertson <josh.robertson@coinclear.uk>
2020-11-16 15:12:25 +0000

    First pass at JS functions for getting & setting a status (WIP)

--- functions/fama/fama.js
+++ functions/fama/fama.js
@@ -2,12 +2,63 @@ require('dotenv').config()
 
 const fetch = require('node-fetch')
 
-// Logic testing
-exports.handler = function(event, context, callback) {
-	return {
-		statusCode: 200,
-		body: 'It is alive!'
+exports.handler = function(event) {
+	// Check the call is from the same site
+	if(event.headers['sec-fetch-site'] !== 'same-origin') {
+		return {
+			statusCode: 401,
+			body: 'You are not authorised to access this function'
+		}
 	}
+
+	// Check for an action in the URL
+	if(!event.queryStringParameters.action) {
+		return {
+			statusCode: 400,
+			body: 'The "action" parameter is missing'
+		}
+	}
+
+	// Build the initial payload
+	const payload = {
+		method: 'GET',
+		headers: {
+			'content-type': 'application/json; charset=utf-8',
+			'authorization': `Bearer ${process.env.SLACK_OAUTH_TOKEN}`
+		}
+	}
+
+	// Set the default endpoint to read
+	let endpoint = 'users.profile.get';
+
+	// Set update option
+	if(event.queryStringParameters.action == 'update') {
+
+		endpoint = 'users.profile.set'
+
+		payload['method'] = 'POST'
+		// Set a predefined status based on a code
+		// 0 = clear, 1 = lunch, 2 = call, 3 = brb, 4 = occupied
+		payload['body'] = JSON.stringify(
+			generateStatus(event.queryStringParameters.statusCode)
+		)
+	}
+
+	// Make the Slack API request
+	return fetch(
+		`https://slack.com/api/${endpoint}`,
+		payload
+	)
+		// Parse the response as JSON
+		.then(data => data.json())
+		.then((data) => ({
+			statusCode: 200,
+			body: JSON.stringify(data)
+		}))
+		.catch(error => ({
+			statusCode: 422,
+			body: `Oops! Something went wrong. ${error}`
+		}))
 }
 
 // Set the status
